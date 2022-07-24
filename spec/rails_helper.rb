ab8447b92dc3d180c0933bfb3d7fe3ce71dd1241
Davide Almeida <davide.jhonatan@gmail.com>
2022-07-24 14:31:24 -0300

    feat: add simplecov and config rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+
+SimpleCov.start 'rails' do
+  add_filter 'channels'
+  add_filter 'helpers'
+  add_filter 'jobs'
+  add_filter 'mailers'
+  add_filter 'serializers'
+end
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
